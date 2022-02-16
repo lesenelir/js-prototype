f5e2e73e423017dc9208f28a07f5746d5b58bcb9
lesenelir <miaomiaobabyzy@gmail.com>
2022-02-16 12:53:10 +0800

    Update using ES6 Class to complete extends

---
+++ src/03-继承模式/03-ES6class实现继承.js
@@ -0,0 +1,47 @@
+/**
+ *  ES6 class 实现继承
+ *
+ */
+
+class Person {
+  constructor(name) {
+    this.name = name
+  }
+
+  drink() {
+    console.log("喝水")
+  }
+}
+
+
+class Student extends Person {
+  constructor(name, score) {
+    super(name)
+    this.score = score
+  }
+
+  introduce() {
+    console.log(`我是${this.name}，考了${this.score}分`)
+  }
+}
+
+
+class Teacher extends Person {
+  constructor(name, subject) {
+    super(name)
+    this.subject = subject
+  }
+
+  teach() {
+    console.log(`我是${this.name}，教${this.subject}课程`)
+  }
+}
+
+
+const student = new Student('张三', 99)
+student.introduce()
+student.drink()
+
+const teacher = new Teacher('李四', 'web develop')
+teacher.teach()
+teacher.drink()
